686cfc20a6ca3019a81571e36b72f9d64130628e
tetianaNEC <tetiana.nec@gmail.com>
2024-06-17 17:27:28 -0400

    lesson_1_1 changes

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import * as React from 'react';
 
+const title = 'Todo List';
+
+const todoList = [
+  {
+   id: 1,
+   title: 'Read the book', 
+  },
+  {
+    id:2,
+    title: 'Complete assignment',
+  },
+  {
+    id: 3,
+    title: 'Wright essay',
+  }
+];
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>{title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <ul>{todoList.map(function(item){
+        return <li key={item.id}>{item.title}</li>
+      })}</ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
+
